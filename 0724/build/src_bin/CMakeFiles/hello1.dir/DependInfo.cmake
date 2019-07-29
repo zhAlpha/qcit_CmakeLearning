0312b377273514c956b9e63918f51daabef4e725
zhuhui1212 <644526179@qq.com>
2019-07-29 17:35:23 +0800

    add new directory

---
+++ 0724/build/src_bin/CMakeFiles/hello1.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/zh/qcit_CmakeLearning/0724/src_bin/usehello.cpp" "/home/zh/qcit_CmakeLearning/0724/build/src_bin/CMakeFiles/hello1.dir/usehello.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src_bin/../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/zh/qcit_CmakeLearning/0724/build/src/CMakeFiles/hello_shared.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ 0724/build/src_bin/CMakeFiles/hello1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello1.dir/usehello.cpp.o"
+  "../../bin/hello1.pdb"
+  "../../bin/hello1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hello1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
